9feb5dfcb37c5257f786230d27d22e896ccf1f91
Sammy Robens-Paradise <srobensparadise@gmail.com>
2019-11-03 21:50:33 -0800

    updated readme

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 > global-winery for grape, a global frontend
 
-[![NPM](https://img.shields.io/npm/v/global-winery.svg)](https://www.npmjs.com/package/global-winery) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+[![NPM](https://img.shields.io/npm/v/global-winery.svg)](https://www.npmjs.com/package/global-winery) [![JavaScript Style Guide](https://img.shields.io/badge/Code%20Style-Prettier-orange)](https://standardjs.com)
 
 ## Install
 
@@ -15,19 +15,37 @@ npm install --save global-winery
 ```jsx
 import React, { Component } from 'react'
 
-import MyComponent from 'global-winery'
+import Button from 'global-winery'
 
 class Example extends Component {
-  render () {
+  render() {
     return (
-      <MyComponent props={'props'} />
+      <Button
+        text='Sign Up'
+        small={true}
+        event={() => alert('Button Clicked!')}
+        hover={true}
+      />
     )
   }
 }
 ```
+## Running Locally
+
+```bash
+git clone https://github.com/SammyRobensParadise/raindrop.git
+```
+```bash
+cd raindrop && npm start
+```
+```bash
+cd raindrop/example && npm start
+```
+## Creator
+
+[webiste](https://sammyrp.com)
+[portfolio](https://sammy.world)
 
 ## License
 
 MIT © [SammyRobensParadise](https://github.com/SammyRobensParadise)
-[webiste](https://sammyrp.com)
-[portfolio](https://sammy.world)
--- example/src/App.js
+++ example/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 import {
   Button,
   MultiButton,
@@ -7,21 +7,21 @@ import {
   FixedInput,
   CheckBox,
   styled
-} from 'global-winery';
-import { Container, Jumbotron } from "reactstrap";
+} from 'global-winery'
+import { Container, Jumbotron } from 'reactstrap'
 
 const Wrapper = styled.div`
-  font-family: "Montserrat", sans-serif;
+  font-family: 'Montserrat', sans-serif;
   font-weight: 100;
   height: 100%;
   margin-left: 0px;
-`;
+`
 const Title = styled.h1`
   text-align: center;
   font-size: 30pt;
   color: #000000;
   font-weight: 100;
-`;
+`
 const SectionGradient = styled.div`
   width: 100%;
   height: 20px;
@@ -33,13 +33,13 @@ const SectionGradient = styled.div`
   );
   margin-left: 0px;
   margin-right: 0px;
-`;
+`
 const SectionTitle = styled.h2`
   text-align: center;
   font-size: 20pt;
   color: #000000;
   font-weight: 100;
-`;
+`
 const Content = styled.div`
 text-align: center
 display: inline-block;
@@ -49,10 +49,10 @@ padding-left: 45px;
   float: left;
 
 }
-`;
+`
 const Code = styled.div`
-  @import url("https://fonts.googleapis.com/css?family=Montserrat|Source+Code+Pro&display=swap");
-  font-family: "Source Code Pro", monospace;
+  @import url('https://fonts.googleapis.com/css?family=Montserrat|Source+Code+Pro&display=swap');
+  font-family: 'Source Code Pro', monospace;
   font-weight: 100;
   border-radius: 3px;
   margin-bottom: 30px;
@@ -60,10 +60,10 @@ const Code = styled.div`
   height: 100%;
   margin-top: 45px;
   text-align: left;
-`;
+`
 const Subsection = styled.p`
   text-align: center;
-`;
+`
 
 const App = () => (
   <Wrapper>
@@ -75,8 +75,8 @@ const App = () => (
         interfaces, built in React.js.
       </Subsection>
       <Subsection>
-        Created with ❤️ by{" "}
-        <a href="https://sammyrp.com">Sammy Robens-Paradise</a> for Grape.
+        Created with ❤️ by{' '}
+        <a href='https://sammyrp.com'>Sammy Robens-Paradise</a> for Grape.
       </Subsection>
     </Jumbotron>
     <SectionGradient></SectionGradient>
@@ -85,11 +85,11 @@ const App = () => (
     <CheckBoxesSection></CheckBoxesSection>
     <IconSection></IconSection>
   </Wrapper>
-);
+)
 
 const ButtonsSection = () => {
-  const [count, setCount] = useState(0);
-  const [countTwo, setCountTwo] = useState(0);
+  const [count, setCount] = useState(0)
+  const [countTwo, setCountTwo] = useState(0)
 
   return (
     <Container>
@@ -100,18 +100,18 @@ const ButtonsSection = () => {
         <Content>
           <Code>Button Props: text,small,event</Code>
           <Button
-            text="Sign Up"
+            text='Sign Up'
             small={true}
-            event={() => alert("Button Clicked!")}
+            event={() => alert('Button Clicked!')}
             hover={true}
           ></Button>
         </Content>
         <Content>
           <Code>Button Props: text,small,event,dropShadow</Code>
           <Button
-            text="Sign Up"
+            text='Sign Up'
             small={true}
-            event={() => alert("Button Clicked!")}
+            event={() => alert('Button Clicked!')}
             dropShadow={true}
             hover={true}
           ></Button>
@@ -119,33 +119,33 @@ const ButtonsSection = () => {
         <Content>
           <Code>Button Props: text,small,event,hover,invert</Code>
           <Button
-            text="Sign Up"
+            text='Sign Up'
             small={true}
             invert={true}
             hover={true}
-            event={() => alert("Button Clicked!")}
+            event={() => alert('Button Clicked!')}
           ></Button>
         </Content>
         <Content>
           <Code>Button Props: text,small,event,dropShadow,hover,invert</Code>
           <Button
-            text="Sign Up"
+            text='Sign Up'
             small={true}
             invert={true}
             hover={true}
             dropShadow={true}
-            event={() => alert("Button Clicked!")}
+            event={() => alert('Button Clicked!')}
           ></Button>
         </Content>
         <Content>
           <Code>
-            {" "}
+            {' '}
             MultiButton Props:
             textLeft,textRight,leftFocus,RightFocus,leftEvent,RightEvent
           </Code>
           <MultiButton
-            textLeft="Left Buttton"
-            textRight="Right Button"
+            textLeft='Left Buttton'
+            textRight='Right Button'
             leftFocus={count % 2 === 0 ? true : false}
             rightFocus={count % 2 === 0 ? false : true}
             leftEvent={() => setCount(count + 1)}
@@ -155,13 +155,13 @@ const ButtonsSection = () => {
         <br></br>
         <Content>
           <Code>
-            {" "}
+            {' '}
             MultiButton Props:
             textLeft,textRight,leftFocus,RightFocus,leftEvent,RightEvent,dropShadow
           </Code>
           <MultiButton
-            textLeft="Left Buttton"
-            textRight="Right Button"
+            textLeft='Left Buttton'
+            textRight='Right Button'
             leftFocus={countTwo % 2 === 0 ? false : true}
             rightFocus={countTwo % 2 === 0 ? true : false}
             leftEvent={() => setCountTwo(countTwo + 1)}
@@ -172,24 +172,24 @@ const ButtonsSection = () => {
         <br></br>
         <Content>
           <Code> Chevron Props: direction, color</Code>
-          <Chevron direction="RIGHT" color="#2EBF91"></Chevron>
+          <Chevron direction='RIGHT' color='#2EBF91'></Chevron>
         </Content>
         <Content>
           <Code> Chevron Props: direction, color</Code>
-          <Chevron direction="LEFT" color="#361E5C"></Chevron>
+          <Chevron direction='LEFT' color='#361E5C'></Chevron>
         </Content>
         <Content>
           <Code> Chevron Props: direction, color</Code>
-          <Chevron direction="UP" color="#361E5C"></Chevron>
+          <Chevron direction='UP' color='#361E5C'></Chevron>
         </Content>
         <Content>
           <Code> Chevron Props: direction, color</Code>
-          <Chevron direction="DOWN" color="#2EBF91"></Chevron>
+          <Chevron direction='DOWN' color='#2EBF91'></Chevron>
         </Content>
       </Container>
     </Container>
-  );
-};
+  )
+}
 
 const InputSection = () => (
   <Container>
@@ -200,23 +200,23 @@ const InputSection = () => (
       <Code>
         Text Input Resizable and Scrollable Props: type,size,scrollAndResize
       </Code>
-      <SingleInput type="text" size="30" scrollAndResize={true}></SingleInput>
+      <SingleInput type='text' size='30' scrollAndResize={true}></SingleInput>
     </Content>
     <br></br>
     <Content>
       <Code>Text Input Resizable Default Props: type,size,scrollAndResize</Code>
-      <SingleInput type="text" size="30" scrollAndResize={false}></SingleInput>
+      <SingleInput type='text' size='30' scrollAndResize={false}></SingleInput>
     </Content>
     <br></br>
     <Content>
       <Code>Text Input Resizable Default Props: type,size,width,height</Code>
-      <FixedInput type="text" size="30" width={800} height={300}></FixedInput>
+      <FixedInput type='text' size='30' width={800} height={300}></FixedInput>
     </Content>
   </Container>
-);
+)
 
 const CheckBoxesSection = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
   return (
     <Container>
       <Jumbotron>
@@ -226,7 +226,7 @@ const CheckBoxesSection = () => {
         <Code>CheckBox props: Checked, borderColor </Code>
         <CheckBox
           checked={count % 2 === 0 ? true : false}
-          borderColor={"#A9A9A9"}
+          borderColor={'#A9A9A9'}
           event={() => setCount(count + 1)}
         />
       </Content>
@@ -234,23 +234,22 @@ const CheckBoxesSection = () => {
         <Code>CheckBox props: Checked, borderColor </Code>
         <CheckBox
           checked={count % 2 === 0 ? false : true}
-          borderColor={"#A9A9A9"}
+          borderColor={'#A9A9A9'}
           event={() => setCount(count + 1)}
         />
       </Content>
     </Container>
-  );
-};
-
-const IconSection =  () => {
-  return (
-    <Container>
-    <Jumbotron>
-      <SectionTitle>Icons</SectionTitle>
-    </Jumbotron>
-    <Content>
-    </Content>
-  </Container>
   )
 }
- export default App
+
+const IconSection = () => {
+  return (
+    <Container>
+      <Jumbotron>
+        <SectionTitle>Icons</SectionTitle>
+      </Jumbotron>
+      <Content></Content>
+    </Container>
+  )
+}
+export default App
