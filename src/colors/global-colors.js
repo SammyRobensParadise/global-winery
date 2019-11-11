2c3b08f4953a58ac4fce6796d6473003e0cf0425
Sammy Robens-Paradise <srobensparadise@gmail.com>
2019-11-10 17:27:00 -0800

    added colors

---
+++ src/colors/global-colors.js
@@ -0,0 +1,29 @@
+const GrapeGreen = '#2EBF91'
+const GrapePurple = '#57437C'
+const GrapeLightGreyText = '#A2A2A2'
+const GrapePlaceholderGrey = '#EDEDED'
+const GrapeBackgroundTab = '#00000029'
+const GrapeWhite = '#ffffff'
+const GrapeBlack = '#000000'
+const GrapeGreyButton = '#959595'
+const GrapeConsole = '#555555'
+const GrapeIDE = '#3C3C3C'
+const GrapeRed = '#FF0000'
+const GrapePurpleGradient =
+  'linear-gradient(90deg, rgba(44,35,62,1) 0%, rgba(51,23,106,1) 100%)'
+const GrapeGreenPurpleGradient = 'linear-gradient(90deg, rgba(38,159,157,1) 0%, rgba(87,67,124,1) 100%);'
+export {
+  GrapeGreen,
+  GrapePurple,
+  GrapeLightGreyText,
+  GrapePlaceholderGrey,
+  GrapeBackgroundTab,
+  GrapeWhite,
+  GrapeBlack,
+  GrapeGreyButton,
+  GrapeConsole,
+  GrapeIDE,
+  GrapeRed,
+  GrapePurpleGradient,
+  GrapeGreenPurpleGradient
+}
